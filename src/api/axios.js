7cf14c827d7474dabbac654f8019cf59e64f2996
Nurseit <nurseitbakytbekov@gmail.com>
2023-06-09 20:35:44 +0600

    test

--- src/api/axios.js
+++ src/api/axios.js
@@ -4,9 +4,18 @@ const BASE_URL = "http://34.159.48.221/auth";
 const API_URL = "http://localhost:3000/";
 
 export default axios.create({
-    withCredentials: true,
     baseURL: BASE_URL,  
     headers: {
       "Content-Type": "application/json"
     }
   });
+
+// export const API_URL = "http://localhost:3000/";
+
+// const $api= axios.create({
+//     withCredentials: true,
+//     baseURL: API_URL
+//   });
+
+
+
